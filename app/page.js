d1c62131da890501709178e9172d73dfe6baa50d
Jake Samuels <samuels242@googlemail.com>
2023-08-04 15:24:39 +0100

    Import Link Grid

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@components/Home/Hero";
+import LinkGrid from "@components/Home/LinkGrid";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LinkGrid />
     </div>
   );
 };
