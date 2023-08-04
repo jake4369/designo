e2fd8237bd72e4a1be703fb25a2648b117aa01a8
Jake Samuels <samuels242@googlemail.com>
2023-08-04 16:23:49 +0100

    Import Features

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@components/Home/Hero";
 import LinkGrid from "@components/Home/LinkGrid";
+import Features from "@components/Home/Features";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LinkGrid />
+      <Features />
     </div>
   );
 };
