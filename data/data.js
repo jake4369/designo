93fede7278513edcaf061af7385e67259d45a594
Jake Samuels <samuels242@googlemail.com>
2023-08-05 02:06:54 +0100

    Page data

---
+++ data/data.js
@@ -0,0 +1,32 @@
+export const webDesignData = [
+  {
+    image: "/assets/web-design/desktop/image-express.jpg",
+    heading: "EXPRESS",
+    text: "A multi-carrier shipping website for ecommerce businesses",
+  },
+  {
+    image: "/assets/web-design/desktop/image-transfer.jpg",
+    heading: "TRANSFER",
+    text: "Site for low-cost money transfers and sending money within seconds",
+  },
+  {
+    image: "/assets/web-design/desktop/image-photon.jpg",
+    heading: "PHOTON",
+    text: "A state-of-the-art music player with high-resolution audio and DSP effects",
+  },
+  {
+    image: "/assets/web-design/desktop/image-builder.jpg",
+    heading: "BUILDER",
+    text: "Connects users with local contractors based on their location",
+  },
+  {
+    image: "/assets/web-design/desktop/image-blogr.jpg",
+    heading: "BLOGR",
+    text: "Blogr is a platform for creating an online blog or publication",
+  },
+  {
+    image: "/assets/web-design/desktop/image-camp.jpg",
+    heading: "CAMP",
+    text: "Get expert training in coding, data, design, and digital marketing",
+  },
+];
